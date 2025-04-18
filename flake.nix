87745a2b963e49577d5e9a119b2029b778214ccb
sobte <i@sobte.me>
2025-04-19 04:19:01 +0800

    feat: add node and rust templates

--- flake.nix
+++ flake.nix
@@ -1,44 +1,29 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixos-unstable (use flakehub to avoid github api limit)
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
+      url = "https://flakehub.com/f/snowfallorg/lib/*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # see: https://github.com/cachix/git-hooks.nix
     pre-commit-hooks = {
-      url = "github:cachix/git-hooks.nix";
+      url = "https://flakehub.com/f/cachix/git-hooks.nix/0.1.*.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
     inputs:
-    let
-      # TODO: write your own module loader with container support.
-      lib = inputs.snowfall-lib.mkLib {
-        # snowfall doc: https://snowfall.org/guides/lib/quickstart/
-        inherit inputs;
-        # root dir
-        src = ./.;
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
 
-        snowfall = {
-          namespace = "example";
-          meta = {
-            name = "example-flake";
-            title = "example' Nix Flakes";
-          };
-        };
+      templates = {
+        rust.description = "Rust development environment";
+        node.description = "Node.js development environment";
       };
-    in
-    lib.mkFlake {
-
-      channels-config = {
-        allowUnfree = true;
-        permittedInsecurePackages = [ ];
-      };
-
-      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-rfc-style; };
     };
 }

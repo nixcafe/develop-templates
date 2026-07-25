62959c2d1f2fc2d86ff549aceae5538809008da6
sobte <i@sobte.me>
2026-07-25 16:08:46 +0800

    chore: update purr to use github:nixcafe/purr

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # nixos-unstable (use flakehub to avoid github api limit)
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    purr.url = "https://flakehub.com/f/nixcafe/purr/0.1.*.tar.gz";
+    purr.url = "github:nixcafe/purr";
 
     # see: https://github.com/cachix/git-hooks.nix
     git-hooks = {
